b3b221fd7798ed0f9873cc1bce342eda0b495565
Miguel da Mota <miguel@damota.de>
2024-04-13 08:54:43 +0200

    refactor: improve auth middleware

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,25 +1,37 @@
 package middleware
 
 import (
+	"log"
+	"strings"
+
 	"github.com/gosquash/api/pkg/structs"
 
 	"github.com/labstack/echo/v4"
 )
 
+// Authentication middleware
 func Authentication(next echo.HandlerFunc) echo.HandlerFunc {
 
 	return func(c echo.Context) error {
 
 		req := c.Request()
 
-		// Check if the user is authenticated
-		// If not, return an error
+		// Check if Authorization header is present
 		token := req.Header.Get("Authorization")
-		if len(token) == 0 {
+		if token == "" {
 			return echo.NewHTTPError(401, "Unauthorized")
 		}
 
-		user := structs.GetUserByToken(token)
+		// Check if Authorization header is in the correct format
+		parts := strings.SplitN(strings.TrimSpace(token), " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			log.Printf("Invalid Authorization Token format: %v", parts)
+			return echo.NewHTTPError(401, "Unauthorized")
+		}
+
+		// Get user by token
+		userToken := parts[1]
+		user := structs.GetUserByToken(userToken)
 		if user == nil {
 			return echo.NewHTTPError(401, "Unauthorized")
 		}
