a38e958687169960353a0db4d67cae6067d58b67
Miguel da Mota <miguel@damota.de>
2024-04-17 09:27:37 +0200

    chore: update deps and db connection string

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,8 +14,8 @@ func Init() {
 	var err error
 
 	dsn := fmt.Sprintf(
-		"postgresql://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), "database", os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+		"postgresql://%s:%s@%s:5432/%s?sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), "database", os.Getenv("DB_NAME"))
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		TranslateError: true,
